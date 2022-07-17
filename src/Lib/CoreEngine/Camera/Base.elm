ba994a5003bb62cdb0134fa1db0668e57010d60c
linsy king <linsy_king@sjtu.edu.cn>
2022-07-17 19:43:40 +0800

    feat: camera control

--- src/Lib/CoreEngine/Camera/Base.elm
+++ src/Lib/CoreEngine/Camera/Base.elm
@@ -5,4 +5,5 @@ type alias CameraData =
     { position : ( Int, Int )
     , velocity : ( Float, Float )
     , visible : ( ( Int, Int ), ( Int, Int ) )
+    , inbox : ( ( Float, Float ), ( Float, Float ) )
     }
