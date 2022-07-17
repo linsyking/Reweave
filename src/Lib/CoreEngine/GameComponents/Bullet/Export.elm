2c65cabcbcfa4c2ded90e3a021501ca1a6a68de3
ZhangJingjing521370910166 <zjingjing@sjtu.edu.cn>
2022-07-17 22:27:18 +0800

    feat: bullet

---
+++ src/Lib/CoreEngine/GameComponents/Bullet/Export.elm
@@ -0,0 +1,16 @@
+module Lib.CoreEngine.GameComponents.Bullet.Export exposing (..)
+
+import Lib.CoreEngine.GameComponent.Base exposing (GameComponent)
+import Lib.CoreEngine.GameComponents.Bullet.Display exposing (view)
+import Lib.CoreEngine.GameComponents.Bullet.Model exposing (initData, initModel, queryModel, updateModel)
+
+
+gameComponent : GameComponent
+gameComponent =
+    { name = "bullet"
+    , data = initData
+    , init = initModel
+    , update = updateModel
+    , view = view
+    , query = queryModel
+    }
