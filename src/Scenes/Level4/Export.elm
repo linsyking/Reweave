b80e955faf40f4ea9d7097ca10688ece54b37230
ZhangJingjing521370910166 <zjingjing@sjtu.edu.cn>
2022-07-20 23:08:59 +0800

    feat: level4

---
+++ src/Scenes/Level4/Export.elm
@@ -0,0 +1,34 @@
+module Scenes.Level4.Export exposing (..)
+
+import Canvas exposing (group)
+import Lib.CoreEngine.Export exposing (Data, genEngineScene, initEngine)
+import Lib.Scene.Base exposing (Scene, SceneMsg(..))
+import Scenes.Level4.Background exposing (background)
+import Scenes.Level4.Config exposing (allChartlets, initActors, initGameGlobalData, initPlayer)
+
+
+game : Int -> SceneMsg -> Data
+game t sm =
+    initEngine t
+        (SceneEngineMsg
+            { player = initPlayer
+            , actors = initActors
+            , chartlets = allChartlets
+            , globalData =
+                initGameGlobalData
+                    (case sm of
+                        SceneEngineTMsg et ->
+                            et.energy
+
+                        _ ->
+                            0
+                    )
+            , background = background
+            , frontground = \_ _ _ -> group [] []
+            }
+        )
+
+
+scene : Scene Data
+scene =
+    genEngineScene game
