38fb1f2e4e1a7924d856d1cad53e4a9a8d5ac6d3
ZhangJingjing521370910166 <zjingjing@sjtu.edu.cn>
2022-07-20 23:15:18 +0800

    feat: level4

---
+++ src/Scenes/Level4/Map.elm
@@ -0,0 +1,15 @@
+module Scenes.Level4.Map exposing (..)
+
+import Array2D
+import Lib.Map.Poly exposing (buildrect, buildrectT)
+
+
+sds : Array2D.Array2D Int
+sds =
+    Array2D.repeat 120 70 0
+
+
+mymap : Array2D.Array2D Int
+mymap =
+    sds
+        |> buildrect ( 0, 0 ) ( 1, 70 ) 1
