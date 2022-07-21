e1f7ea956d445482150c4b4ecf9c2d7d22732e86
ZhangJingjing521370910166 <zjingjing@sjtu.edu.cn>
2022-07-21 23:36:44 +0800

    fix: add spike

--- src/Scenes/Level4/Config.elm
+++ src/Scenes/Level4/Config.elm
@@ -40,8 +40,8 @@ initPlayer : Int -> PlayerInitPosition -> GameComponent
 initPlayer t pos =
     case pos of
         DefaultPlayerPosition ->
-            --initGameComponent t (GamePlayerInit (PlayerInit ( 50, 2000 ))) Player.gameComponent
-            initGameComponent t (GamePlayerInit (PlayerInit ( 3744, 2000 ))) Player.gameComponent
+            initGameComponent t (GamePlayerInit (PlayerInit ( 50, 2000 ))) Player.gameComponent
+            --initGameComponent t (GamePlayerInit (PlayerInit ( 3744, 2000 ))) Player.gameComponent
 
         CustomPlayerPosition x ->
             initGameComponent t (GamePlayerInit (PlayerInit x)) Player.gameComponent
