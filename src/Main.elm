d1e2cd20a06a5b4664ac2990fd5169304c2b1f66
linsyking <linsyking@sjtu.edu.cn>
2022-07-24 15:49:07 +0800

    fix: startpoint home

--- src/Main.elm
+++ src/Main.elm
@@ -75,7 +75,7 @@ init : Flags -> ( Model, Cmd Msg, AudioCmd Msg )
 init flags =
     let
         ms =
-            loadSceneByName initModel "Level1" NullSceneMsg
+            loadSceneByName initModel "Home" NullSceneMsg
 
         oldgd =
             ms.currentGlobalData
